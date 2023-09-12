05218f7a161a854d4056ecb940b0d87593d33e82
Keval Shah <kevals2005@gmail.com>
2023-09-11 19:11:49 -0500

    first commit; created tele-op code for 4wd using okapilib

---
+++ include/constants.h
@@ -0,0 +1,11 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+namespace constants
+{
+    inline constexpr int fl_port = 1;
+    inline constexpr int fr_port = 2;
+    inline constexpr int bl_port = 3;
+    inline constexpr int br_port = 4;
+}
+#endif
